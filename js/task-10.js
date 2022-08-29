1802e80fb0ee5b5df2eb461f3e6d113aad4c52ce
shevalie30 <103785982+shevalie30@users.noreply.github.com>
2022-08-29 18:48:39 +0300

    ......

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const controls = document.querySelector('#controls');
+const boxesContainer = document.querySelector('#boxes');
+
+function createBoxes(amount = 1) {
+  const boxes = []
+  for (let i = 0; i < amount; i++) {
+    let size = 30
+    if (i > 0) size = (i * 10) + size
+    const box = document.createElement('div')
+    box.style.width = `${size}px`
+    box.style.height = `${size}px`
+    box.style.backgroundColor = getRandomHexColor()
+    boxes.push(box)
+  };
+  boxesContainer.append(...boxes)
+};
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = ''
+};
+
+controls.addEventListener('click', e => {
+  let amount = controls.querySelector('input').value
+  if ('create' in e.target.dataset) createBoxes(amount)
+  if ('destroy' in e.target.dataset) destroyBoxes()
+});
